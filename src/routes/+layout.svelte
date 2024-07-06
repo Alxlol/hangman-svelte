c554957aba877146eb9fdd6d70180f7639444b53
Alexander <friberg.alx@gmail.com>
2024-07-07 01:50:40 +0200

    yes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center bg-neutral-950 text-white">
+<div class="flex min-h-screen flex-col bg-neutral-950 text-white">
 	<slot></slot>
 </div>
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -5,39 +5,74 @@
 	const secretWord = data.word;
 	let selectedLetters: string[] = [];
 	let lives: number = 8;
+	let gameState: 'play' | 'won' | 'over' = 'play';
 
 	function selectLetter(letter: string) {
 		selectedLetters.push(letter);
 		selectedLetters = [...selectedLetters];
 
 		if (secretWord.includes(letter)) {
-			console.log('Letter is in word');
+			let allLettersGuessed: boolean = true;
+			secretWord.split('').forEach((element) => {
+				if (!selectedLetters.includes(element)) {
+					allLettersGuessed = false;
+					return;
+				}
+			});
+			if (allLettersGuessed) {
+				gameState = 'won';
+			}
 		} else {
-			console.log('wrong');
+			lives--;
+			if (lives <= 0) {
+				gameState = 'over';
+			}
 		}
 	}
 </script>
 
-<!-- <p>Word: {secretWord}</p> -->
-<div class="flex items-center gap-20 px-36">
-	<div class="flex flex-1 items-center justify-center gap-2 text-6xl">
-		{#each [...secretWord] as character}
-			{#if selectedLetters.includes(character)}
-				<p class="uppercase">{character}</p>
-			{:else}
-				<p>_</p>
-			{/if}
-		{/each}
-	</div>
-	<div class="flex max-w-[66rem] flex-1 flex-wrap items-center justify-center gap-8 text-3xl">
-		{#each [...alphabet] as letter}
-			<button
-				disabled={selectedLetters.includes(letter)}
-				on:click={() => selectLetter(letter)}
-				class="h-14 w-14 rounded-full bg-blue-500 uppercase hover:bg-blue-400
+{#if gameState === 'play'}
+	<div class="flex flex-col items-center gap-20 px-36">
+		<div class="mt-20">
+			<p>HP: {lives}</p>
+		</div>
+		<div class="flex items-center justify-center gap-4 text-7xl">
+			{#each [...secretWord] as character}
+				{#if selectedLetters.includes(character)}
+					<p class="uppercase">{character}</p>
+				{:else}
+					<p>_</p>
+				{/if}
+			{/each}
+		</div>
+		<div class="flex max-w-[50rem] flex-wrap items-center justify-center gap-8 text-3xl">
+			{#each [...alphabet] as letter}
+				<button
+					disabled={selectedLetters.includes(letter)}
+					on:click={() => selectLetter(letter)}
+					class="h-14 w-14 rounded-full bg-blue-500 uppercase hover:bg-blue-400
                 {selectedLetters.includes(letter) ? 'bg-gray-500 hover:bg-gray-500' : ''}">
-				{letter}
-			</button>
-		{/each}
+					{letter}
+				</button>
+			{/each}
+		</div>
 	</div>
-</div>
+{:else if gameState === 'over'}
+	<div class="flex min-h-screen flex-col items-center justify-center gap-4">
+		<p class="text-4xl font-bold">GAME OVER</p>
+		<button
+			class="rounded-md bg-orange-500 px-4 py-2"
+			on:click={() => {
+				location.reload();
+			}}>Try Again</button>
+	</div>
+{:else if gameState === 'won'}
+	<div class="flex min-h-screen flex-col items-center justify-center gap-4">
+		<p class="text-4xl font-bold">YOU WON 😁</p>
+		<button
+			class="rounded-md bg-orange-500 px-4 py-2"
+			on:click={() => {
+				location.reload();
+			}}>Play again</button>
+	</div>
+{/if}
